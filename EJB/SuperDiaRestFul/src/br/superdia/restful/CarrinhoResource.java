7333c9a4e74883cab2bed3c9d6ace6bb752974d1
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-12-22 13:29:29 -0200

    Api restfull carrinho

--- EJB/SuperDiaRestFul/src/br/superdia/restful/CarrinhoResource.java
+++ EJB/SuperDiaRestFul/src/br/superdia/restful/CarrinhoResource.java
@@ -4,24 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.google.gson.Gson;
 
-import br.superdia.modelo.Produto;
+import br.superdia.modelo.ItemVenda;
 import br.superdia.sessionbean.ICarrinho;
 
 @Path("carrinho")
 public class CarrinhoResource {
 	private Gson gson = new Gson();
-	private List<Produto> produtos = new ArrayList<>();
+	private List<ItemVenda> itens = new ArrayList<>();
 	
 	@EJB
 	private ICarrinho carrinho;
 	
 	@Path("inserir")
-	public void addProduct(String product){
-		Produto p = gson.fromJson(product, Produto.class);
-		produtos.add(p);
+	@POST
+	@Consumes("application/json")
+	public void addItem(String item){
+		ItemVenda itemVenda = gson.fromJson(item, ItemVenda.class);
+		itens.add(itemVenda);
+	}
+	
+	@Path("remover/{id}")
+	@DELETE
+	@Consumes("application/json")
+	public void removeItem(final @PathParam("id") String id){
+		itens.removeIf(i -> i.getId() == Long.valueOf(id));
+	}
+	
+	@Path("listar")
+	@GET
+	@Produces("application/json")
+	public String getAll(){
+		return gson.toJson(itens.toString());
 	}
 }
