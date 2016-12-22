aa7e727759b93b002b6cb6ccfcb3f55d1e6d550c
SaWinchester <samukasaww@live.com>
2016-12-22 14:50:48 -0200

    Add WebService Valida Cartão no ClienteSE

--- EJB/SuperDiaEJB/ejbModule/br/superdia/webservice/ValidateCard.java
+++ EJB/SuperDiaEJB/ejbModule/br/superdia/webservice/ValidateCard.java
@@ -4,14 +4,14 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
-import br.superdia.sessionbean.ValidaCompraBean;
+import br.superdia.sessionbean.IValidaCompra;
 
 @WebService
 @Stateless
 public class ValidateCard {
 
 	@EJB
-	private ValidaCompraBean validaCompraBean;
+	private IValidaCompra validaCompraBean;
 	
 	public Boolean validaCartao(String bandeiraCartao, String numeroCartao) {
 		return validaCompraBean.validaCartao(bandeiraCartao, numeroCartao);
