9c8ff8773b1e7919af06d844e97b2717ce8cbbc7
SaWinchester <samukasaww@live.com>
2016-12-23 13:03:04 -0200

    Correção do Cadastrar Usuario

--- EJB/SuperDiaWeb/src/br/superdia/managebean/UsuarioMB.java
+++ EJB/SuperDiaWeb/src/br/superdia/managebean/UsuarioMB.java
@@ -3,10 +3,9 @@ package br.superdia.managebean;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
+import br.superdia.modelo.Perfil;
 import br.superdia.modelo.Usuario;
-import br.superdia.sessionbean.IDAO;
 import br.superdia.sessionbean.IUsuarioDAO;
 import br.superdia.utils.SessionUtil;
 
@@ -16,28 +15,11 @@ import br.superdia.utils.SessionUtil;
 @ManagedBean
 @SessionScoped
 public class UsuarioMB {
-	@EJB
-	private IDAO<Usuario> iUsuario;
 	
 	@EJB
 	private IUsuarioDAO usuarioDAO;
 	
 	private Usuario usuario = new Usuario();
-	
-	private String senha2;
-	
-	public String getSenha2() {
-		return senha2;
-	}
-
-	public void setSenha2(String senha2) {
-		this.senha2 = senha2;
-	}
-
-	public void addUsuario(){
-		if(usuario.getId()==null)
-			iUsuario.add(usuario);
-	}
 	
 	public String login(){
 		boolean loginValido = usuarioDAO.isValid(usuario);
@@ -47,7 +29,7 @@ public class UsuarioMB {
 			
 			SessionUtil.setParam("USUARIOLogado", usuario);
 			
-			if(usuario.getPerfil().equalsIgnoreCase("adm"))
+			if(usuario.getPerfil().equalsIgnoreCase(Perfil.ADMINISTRADOR.getPerfil()))
 				return "controleEstoque";
 			else
 				return "produtos";
@@ -77,27 +59,6 @@ public class UsuarioMB {
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
-	}
-	
-	public String cadastrarCliente(){
-		
-		// Verificar se o cliente ja existe no banco.
-		//				...........
-		
-		// Verifica se as duas senhas batem.
-		if(senha2.equalsIgnoreCase(usuario.getSenha())){
-			
-			usuario.setPerfil("cliente");
-			iUsuario.add(usuario);
-			
-			usuario = new Usuario();
-			
-			return "login";
-			
-		}
-		
-		return "cadastrarCliente";
-		
 	}
 	
 }
