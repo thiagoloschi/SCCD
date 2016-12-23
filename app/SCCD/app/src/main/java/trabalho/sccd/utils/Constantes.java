326a57d09c7c02d977501e27c5f6886efbf012b1
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2016-12-23 01:54:49 -0200

    editado ip api

--- app/SCCD/app/src/main/java/trabalho/sccd/utils/Constantes.java
+++ app/SCCD/app/src/main/java/trabalho/sccd/utils/Constantes.java
@@ -4,6 +4,6 @@ public final class Constantes {
 
     private Constantes(){}
 
-    public static final String URL_API = "http://10.3.1.100:8080";
+    public static final String URL_API = "http://192.168.0.101:8080";
     public static final String URL_API_LISTAR_PRODUTOS = URL_API + "/SuperDiaRestFul/rest/produtos/listar?token=123456";
 }
